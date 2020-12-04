f2a18a9e74855511b5feec3f960da124cb0ffc06
Thibault Fischer <thibault.fischer@altran.com>
2020-12-04 14:55:58 +0100

    4

--- first-test.js
+++ first-test.js
@@ -6,7 +6,16 @@ function addWithIf(a, b) {
     if (a > 4) {
         return a + b;
     } else {
-        return a/b;
+        return a / b;
+    }
+
+}
+
+function objectTest(param1) {
+    if (param1.name === 'test') {
+        return 'name is test'
+    } else {
+        return 'name is not test'
     }
 
 }
